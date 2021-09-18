6ebbe3c176c78f916d0d74a537b43af4dbd4acdb
Roliga <roliga.here@gmail.com>
2021-09-18 08:17:18 +0200

    ScriptUtils: Add scripting utilities

---
+++ Editor/ScriptUtils.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UnityUtils
+{
+    public static class ScriptUtils
+    {
+        public static string GetTransformPath(Transform transform)
+        {
+            string path = "/" + transform.name;
+            while (transform.parent != null)
+            {
+                transform = transform.parent;
+                path = "/" + transform.name + path;
+            }
+            return path;
+        }
+    }
+}
