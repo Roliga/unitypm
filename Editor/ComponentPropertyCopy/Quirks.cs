c8077e0272136e84bd734115420a1e22464c2d3b
Roliga <roliga.here@gmail.com>
2021-09-18 08:18:20 +0200

    ComponentPropertyCopy: Add Bone Mappings property

--- Editor/ComponentPropertyCopy/Quirks.cs
+++ Editor/ComponentPropertyCopy/Quirks.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityUtils;
 
 namespace ComponentPropertyCopy
 {
@@ -28,10 +29,48 @@ namespace ComponentPropertyCopy
 							outMesh.SetBlendShapeWeight(toIndex, inMesh.GetBlendShapeWeight(i));
 					}
 				},
-				["Bones"] = (inComp, outComp) =>
+				["Bone Mappings"] = (inComp, outComp) =>
 				{
 					SkinnedMeshRenderer inMesh = (SkinnedMeshRenderer)inComp;
 					SkinnedMeshRenderer outMesh = (SkinnedMeshRenderer)outComp;
+
+					Transform FindTransform (Transform transform)
+                    {
+						foreach (Transform it in inMesh.bones)
+							if (transform.name == it.name)
+								return it;
+						return null;
+					}
+
+					int count = 0;
+					List<Transform> bones = new List<Transform>();
+					string switches = "", kept = "";
+
+					foreach (Transform ob in outMesh.bones)
+                    {
+						Transform ib = FindTransform(ob);
+
+						if (ib == null)
+						{
+							bones.Add(ob);
+							kept += $"{ScriptUtils.GetTransformPath(ob)}\n";
+						}
+						else
+						{
+							bones.Add(ib);
+							switches += $"{ScriptUtils.GetTransformPath(ob)} --> {ScriptUtils.GetTransformPath(ib)}\n";
+							count++;
+                        }
+					}
+
+					outMesh.bones = bones.ToArray();
+					Debug.Log($"Switched {count} out of {outMesh.bones.Length} bones on {outMesh.name}\n" +
+						$"\n" +
+						$"Switched:\n" +
+						switches +
+						$"\n" +
+						$"Kept:\n" +
+						kept);
 				}
 			},
 			[typeof(MeshCollider)] = new QuirkList()
