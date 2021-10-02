09c2da40ec551c16c7473166203c6744c00a1b48
Roliga <roliga.here@gmail.com>
2021-10-02 18:18:18 +0200

    UnityPM: Make install queue actually work
    Heck Unity

---
+++ Editor/UnityPM/ImportPackageWithCallback.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using System.Reflection;
+
+// 
+// The usual ImportPackage function is asynchronous if run in interactive mode.
+// There's no way to reliably get a callback when it's done. There are the
+// AssetDatabase.ImportPackage* events, but those are incomplete and don't take
+// into account if you close the import window for example.
+//
+// So let's just commit all the crimes and make our own ImportPackage function.
+//
+// This is all such a mess. Heck Unity.
+// 
+
+namespace UnityUtils.UnityPM
+{
+    static class ImportPackageWithCallback
+    {
+        //
+        // WindowsReordered event
+        // From https://github.com/kirurobo/UniWinApiAsset/blob/master/Scripts/WindowController.cs#L996
+        //
+
+        #region WindowsReordered
+
+        private const BindingFlags BINDING_ATTR = BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic;
+
+        private static readonly FieldInfo m_info = typeof(EditorApplication).GetField("windowsReordered", BINDING_ATTR);
+
+        public static EditorApplication.CallbackFunction windowsReordered
+        {
+            get
+            {
+                return m_info.GetValue(null) as EditorApplication.CallbackFunction;
+            }
+            set
+            {
+                var functions = m_info.GetValue(null) as EditorApplication.CallbackFunction;
+                functions += value;
+                m_info.SetValue(null, functions);
+            }
+        }
+
+        #endregion WindowsReordered
+
+        //
+        // PackageImport editor window type and methods
+        // Mostly from https://assetstore.unity.com/packages/tools/utilities/package2folder-64829
+        //
+
+        #region PackageImport
+
+        private static Type packageImportType;
+        private static Type PackageImportType
+        {
+            get
+            {
+                if (packageImportType == null)
+                {
+                    packageImportType = typeof(MenuItem).Assembly.GetType("UnityEditor.PackageImport");
+                }
+                return packageImportType;
+            }
+        }
+
+        private static MethodInfo validateInputMethodInfo;
+        private static MethodInfo ValidateInputMethodInfo
+        {
+            get
+            {
+                if (validateInputMethodInfo == null)
+                {
+                    validateInputMethodInfo = PackageImportType.GetMethod("ValidateInput", BindingFlags.Static | BindingFlags.NonPublic);
+                }
+
+                return validateInputMethodInfo;
+            }
+        }
+
+        private static bool ValidateInput(object[] items)
+        {
+            return (bool)ValidateInputMethodInfo.Invoke(null, new object[] { items });
+        }
+
+        private static MethodInfo initMethodInfo;
+        private static MethodInfo InitMethodInfo
+        {
+            get
+            {
+                if (initMethodInfo == null)
+                {
+                    initMethodInfo = PackageImportType.GetMethod("Init", BindingFlags.NonPublic | BindingFlags.Instance);
+                }
+
+                return initMethodInfo;
+            }
+        }
+
+        #endregion PackageImport
+
+        //
+        // PackageUtility type + methods
+        // Mostly from https://assetstore.unity.com/packages/tools/utilities/package2folder-64829
+        //
+
+        #region PackageUtility
+
+        private static Type packageUtilityType;
+        private static Type PackageUtilityType
+        {
+            get
+            {
+                if (packageUtilityType == null)
+                {
+                    packageUtilityType = typeof(MenuItem).Assembly.GetType("UnityEditor.PackageUtility");
+                }
+                return packageUtilityType;
+            }
+        }
+
+        private delegate object[] ExtractAndPrepareAssetListDelegate(string packagePath, out string packageIconPath, out string packageManagerDependenciesPath);
+
+        private static ExtractAndPrepareAssetListDelegate extractAndPrepareAssetList;
+        private static ExtractAndPrepareAssetListDelegate ExtractAndPrepareAssetList
+        {
+            get
+            {
+                if (extractAndPrepareAssetList == null)
+                {
+                    var method = PackageUtilityType.GetMethod("ExtractAndPrepareAssetList");
+                    if (method == null)
+                    {
+                        throw new Exception("Couldn't extract method with ExtractAndPrepareAssetListDelegate delegate!");
+                    }
+
+                    extractAndPrepareAssetList = (ExtractAndPrepareAssetListDelegate)Delegate.CreateDelegate(
+                       typeof(ExtractAndPrepareAssetListDelegate),
+                       null,
+                       method);
+                }
+
+                return extractAndPrepareAssetList;
+            }
+        }
+
+        #endregion PackageUtility
+
+        private static List<(EditorWindow, Action)> windowCallbacks = new List<(EditorWindow, Action)>();
+
+        public static void ImportPackage(string packagePath, bool interactive, Action callback)
+        {
+            if (interactive)
+            {
+                // From https://assetstore.unity.com/packages/tools/utilities/package2folder-64829
+                string packageIconPath;
+                string packageManagerDependenciesPath;
+                var assetsItems = ExtractAndPrepareAssetList(packagePath, out packageIconPath, out packageManagerDependenciesPath);
+
+                if (assetsItems == null)
+                {
+                    callback.Invoke();
+                    return;
+                }
+
+                // Emulate ShowImportPackage on PackageImport window
+                // See https://github.com/Unity-Technologies/UnityCsReference/blob/61f92bd79ae862c4465d35270f9d1d57befd1761/Editor/Mono/GUI/PackageImport.cs#L53
+                if (!ValidateInput(assetsItems))
+                {
+                    callback.Invoke();
+                    return;
+                }
+                
+                dynamic w = Convert.ChangeType(EditorWindow.GetWindow(PackageImportType, true, "Import Unity Package"), PackageImportType);
+                InitMethodInfo.Invoke(w, new object[] { packagePath, assetsItems, packageIconPath });
+
+                windowCallbacks.Add((w, callback));
+            }
+            else
+            {
+                AssetDatabase.ImportPackage(packagePath, false);
+                callback.Invoke();
+            }
+        }
+
+        static ImportPackageWithCallback()
+        {
+            windowsReordered += () =>
+            {
+                for (int i = windowCallbacks.Count - 1; i >= 0; i--)
+                {
+                    if (windowCallbacks[i].Item1 == null)
+                    {
+                        windowCallbacks[i].Item2.Invoke();
+                        windowCallbacks.RemoveAt(i);
+                    }
+                }
+            };
+        }
+    }
+}
--- Editor/UnityPM/UnityPM.cs
+++ Editor/UnityPM/UnityPM.cs
@@ -18,6 +18,7 @@ namespace UnityUtils.UnityPM
         UnityPMTab tab = UnityPMTab.Packages;
 
         List<Installable> installQueue = new List<Installable>();
+        Dictionary<Installable, InstallStatus> installQueueStatus;
         ReorderableList installQueueList;
         bool dontAsk = false;
         string downloadFileDirectory = "/";
@@ -32,6 +33,14 @@ namespace UnityUtils.UnityPM
         {
             Packages,
             Settings
+        }
+
+        private enum InstallStatus
+        {
+            Downloading,
+            Installing,
+            Installed,
+            Failed
         }
 
         [MenuItem("Tools/Unity Utils/UnityPM")]
@@ -53,7 +62,18 @@ namespace UnityUtils.UnityPM
                 },
                 drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
                 {
-                    EditorGUI.LabelField(rect, installQueue[index].Name);
+                    Installable installable = installQueue[index];
+                    Rect statusRect = rect;
+                    statusRect.xMin = rect.xMax - 50;
+                    statusRect.xMax = rect.xMax;
+
+                    EditorGUI.LabelField(rect, installable.Name);
+
+                    if(installQueueStatus != null && installQueueStatus.ContainsKey(installable))
+                    {
+                        string status = Enum.GetName(typeof(InstallStatus), installQueueStatus[installable]);
+                        EditorGUI.LabelField(statusRect, status);
+                    }
                 }
             };
         }
@@ -65,9 +85,18 @@ namespace UnityUtils.UnityPM
 
         private void TestStuff()
         {
-            Sources.GumroadSource source = new Sources.GumroadSource();
-            List<Package> p = source.GetPackages();
-
+            //Sources.GumroadSource source = new Sources.GumroadSource();
+            //List<Package> p = source.GetPackages();
+            foreach(EditorWindow w in Resources.FindObjectsOfTypeAll<EditorWindow>())
+            {
+                if(w.titleContent.text == "Import Unity Package")
+                {
+                    Debug.Log(w.GetType().Name);
+                }
+            }
+            //Debug.Log("AAAA");
+            //CodeStage.PackageToFolder.Package2Folder.ImportPackageToFolder(@"C:\Users\Roliga\Desktop\hmm.unitypackage", "Import", true);
+            //Debug.Log("Done");
         }
 
         void DrawPackages()
@@ -160,37 +189,61 @@ namespace UnityUtils.UnityPM
                 downloadFileDirectory = EditorGUILayout.TextField("Download Files To:", downloadFileDirectory);
             if (GUILayout.Button("Install"))
             {
-                foreach (File file in installQueue.OfType<File>())
-                {
-                    if (settings.UseCache)
-                    {
-
-                    }
-                    else
-                    {
-                        string dstDir = $"{Application.dataPath}/{downloadFileDirectory}/";
-                        Directory.CreateDirectory(dstDir);
-                        string dstPath = $"{dstDir}/{file.GetFileName()}";
-                        file.GetWebClient().DownloadFile(file.GetURI(), dstPath);
-                    }
-                }
-                foreach (UnityPackage file in installQueue.OfType<UnityPackage>())
-                {
-                    if(settings.UseCache)
-                    {
-
-                    }
-                    else
-                    {
-                        string tmpPath = $"{settings.CacheDirectory}/downloading";
-                        file.GetWebClient().DownloadFile(file.GetURI(), tmpPath);
-                        AssetDatabase.ImportPackage(tmpPath, !dontAsk);
-                    }
-                    //file.Download(dontAsk);
-                }
-                AssetDatabase.Refresh();
+                installQueueStatus = new Dictionary<Installable, InstallStatus>();
+                //foreach (Installable installable in installQueue)
+                //    installQueueStatus.Add(installable, false);
+                installQueue.GetEnumerator().Dispose();
+                InstallNext(true, installQueue.ToList().GetEnumerator());
             }
             GUILayout.EndVertical();
+        }
+
+        private void InstallNext(bool success, List<Installable>.Enumerator enumerator)
+        {
+            if (enumerator.Current != null)
+            {
+                if (success)
+                    installQueueStatus[enumerator.Current] = InstallStatus.Installed;
+                else
+                    installQueueStatus[enumerator.Current] = InstallStatus.Failed;
+            }
+
+            if (!enumerator.MoveNext())
+                return;
+            installQueueStatus[enumerator.Current] = InstallStatus.Downloading;
+
+            if(enumerator.Current is File)
+            {
+                File file = (File)enumerator.Current;
+                if (settings.UseCache)
+                {
+                    
+                }
+                else
+                {
+                    string dstDir = $"{Application.dataPath}/{downloadFileDirectory}/";
+                    Directory.CreateDirectory(dstDir);
+                    string dstPath = $"{dstDir}/{file.GetFileName()}";
+                    file.GetWebClient().DownloadFile(file.GetURI(), dstPath);
+                }
+                InstallNext(true, enumerator);
+            }
+            else if (enumerator.Current is UnityPackage)
+            {
+                UnityPackage file = (UnityPackage)enumerator.Current;
+                if (settings.UseCache)
+                {
+
+                }
+                else
+                {
+                    string tmpPath = $"{settings.CacheDirectory}/downloading";
+                    file.GetWebClient().DownloadFile(file.GetURI(), tmpPath);
+                    installQueueStatus[enumerator.Current] = InstallStatus.Installing;
+
+                    ImportPackageWithCallback.ImportPackage(tmpPath, !dontAsk, () => { InstallNext(true, enumerator); });
+                }
+            }
         }
 
         void OnGUI()
